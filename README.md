091bee9bfd7134c6e0d7a0cf5f6b040c9ee9114b
Lucas Lee <ldl5@sfu.ca>
2023-07-27 22:33:29 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# CMPT-353-Final-Project
+# Analyzing Reddit Activity from Manga Subreddits
+
+This repository is the final project for Simon Fraser University's *CMPT 353: Computational Data Science (Summer 2023)* by [Ike Chan](#authors) and [Lucas Lee](#authors).
+
+The goal of our project is to use the Reddit Submissions and Comments cluster to answer the question:
+
+> ***Are manga subreddits more active when anime adaptations are made?***
+
+In this project, we applied statistical models to analyze shifts in p-values to determine whether it matches with our hypotheses or not. Our hypotheses are:
+
+**Null Hypothesis**: There is no difference in the amount of activity in a subreddit when anime adaptation based on a manga is released.
+
+**Alternate Hypothesis**: There is a difference in the amount of activity in a subreddit when anime adaptation based on a manga is released.
+
+## Setup
+
+You will need the following tools to run the program:
+
+- Python (3.8+)
+- Spark DataFrames (3.4.1+)
+- [JikanPy](https://github.com/abhinavk99/jikanpy): A Python wrapper for Jikan, an unofficial PHP API for MyAnimeList which holds anime release dates and more
+
+## Authors
+
+- **Ike Chan**
+- **Lucas Lee**
---
+++ collect_data.py
@@ -0,0 +1,96 @@
+import sys
+assert sys.version_info >= (3, 8)
+from pyspark.sql import SparkSession, functions, types, Row
+
+reddit_submissions_path = '/courses/datasets/reddit_submissions_repartitioned/'
+reddit_comments_path = '/courses/datasets/reddit_comments_repartitioned/'
+output = 'reddit-subset'
+
+comments_schema = types.StructType([
+    types.StructField('archived', types.BooleanType()),
+    types.StructField('author', types.StringType()),
+    types.StructField('author_flair_css_class', types.StringType()),
+    types.StructField('author_flair_text', types.StringType()),
+    types.StructField('body', types.StringType()),
+    types.StructField('controversiality', types.LongType()),
+    types.StructField('created_utc', types.StringType()),
+    types.StructField('distinguished', types.StringType()),
+    types.StructField('downs', types.LongType()),
+    types.StructField('edited', types.StringType()),
+    types.StructField('gilded', types.LongType()),
+    types.StructField('id', types.StringType()),
+    types.StructField('link_id', types.StringType()),
+    types.StructField('name', types.StringType()),
+    types.StructField('parent_id', types.StringType()),
+    types.StructField('retrieved_on', types.LongType()),
+    types.StructField('score', types.LongType()),
+    types.StructField('score_hidden', types.BooleanType()),
+    types.StructField('subreddit', types.StringType()),
+    types.StructField('subreddit_id', types.StringType()),
+    types.StructField('ups', types.LongType()),
+    types.StructField('year', types.IntegerType()),
+    types.StructField('month', types.IntegerType()),
+])
+
+submissions_schema = types.StructType([
+    types.StructField('archived', types.BooleanType()),
+    types.StructField('author', types.StringType()),
+    types.StructField('author_flair_css_class', types.StringType()),
+    types.StructField('author_flair_text', types.StringType()),
+    types.StructField('created', types.LongType()),
+    types.StructField('created_utc', types.StringType()),
+    types.StructField('distinguished', types.StringType()),
+    types.StructField('domain', types.StringType()),
+    types.StructField('downs', types.LongType()),
+    types.StructField('edited', types.BooleanType()),
+    types.StructField('from', types.StringType()),
+    types.StructField('from_id', types.StringType()),
+    types.StructField('from_kind', types.StringType()),
+    types.StructField('gilded', types.LongType()),
+    types.StructField('hide_score', types.BooleanType()),
+    types.StructField('id', types.StringType()),
+    types.StructField('is_self', types.BooleanType()),
+    types.StructField('link_flair_css_class', types.StringType()),
+    types.StructField('link_flair_text', types.StringType()),
+    types.StructField('media', types.StringType()),
+    types.StructField('name', types.StringType()),
+    types.StructField('num_comments', types.LongType()),
+    types.StructField('over_18', types.BooleanType()),
+    types.StructField('permalink', types.StringType()),
+    types.StructField('quarantine', types.BooleanType()),
+    types.StructField('retrieved_on', types.LongType()),
+    types.StructField('saved', types.BooleanType()),
+    types.StructField('score', types.LongType()),
+    types.StructField('secure_media', types.StringType()),
+    types.StructField('selftext', types.StringType()),
+    types.StructField('stickied', types.BooleanType()),
+    types.StructField('subreddit', types.StringType()),
+    types.StructField('subreddit_id', types.StringType()),
+    types.StructField('thumbnail', types.StringType()),
+    types.StructField('title', types.StringType()),
+    types.StructField('ups', types.LongType()),
+    types.StructField('url', types.StringType()),
+    types.StructField('year', types.IntegerType()),
+    types.StructField('month', types.IntegerType()),
+])
+
+def main():
+    spark = SparkSession.builder.appName("reddit data").getOrCreate()
+    assert spark.version >= '3.2'
+    spark.sparkContext.setLogLevel('WARN')
+
+    reddit_submissions = spark.read.json(reddit_submissions_path, schema=submissions_schema)
+    reddit_comments = spark.read.json(reddit_comments_path, schema=comments_schema)
+    
+    subs = []
+    # subs = ['Genealogy', 'xkcd', 'optometry', 'Cameras', 'scala']
+    subs = list(map(functions.lit, subs))
+    
+    reddit_submissions.where(reddit_submissions['subreddit'].isin(subs)) \
+        .where(reddit_submissions['year'] == 2022) \
+        .write.json(output + '/submissions', mode='overwrite', compression='gzip')
+    reddit_comments.where(reddit_comments['subreddit'].isin(subs)) \
+        .where(reddit_comments['year'] == 2022) \
+        .write.json(output + '/comments', mode='overwrite', compression='gzip')
+    
+main()
