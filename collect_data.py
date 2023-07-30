4c2c60984ae62745a75a508d62004db9c998717d
Lucas Lee <ldl5@sfu.ca>
2023-07-29 17:31:41 -0700

    Used JikanPy to get animes

--- collect_data.py
+++ collect_data.py
@@ -1,4 +1,6 @@
 import sys
+import time
+from jikanpy import Jikan
 assert sys.version_info >= (3, 8)
 from pyspark.sql import SparkSession, functions, types, Row
 
@@ -74,23 +76,79 @@ submissions_schema = types.StructType([
     types.StructField('month', types.IntegerType()),
 ])
 
+anime_schema = types.StructType([
+    types.StructField('subreddit', types.StringType()),
+    types.StructField('year', types.IntegerType()),
+    types.StructField('month', types.IntegerType()),
+])
+
+def insert_jikan_anime(code, wait_time=0):
+    jikan = Jikan()
+    anime = jikan.anime(code)
+    time.sleep(wait_time)
+    return anime
+
+def filter_date(date):
+    date = date.split("-")
+    year = int(date[0])
+    month = int(date[1])
+    return year, month
+
 def main():
     spark = SparkSession.builder.appName("reddit data").getOrCreate()
     assert spark.version >= '3.2'
     spark.sparkContext.setLogLevel('WARN')
-
+    
+    # Testing for reddit-subset in local data
+    reddit_submissions = spark.read.json("reddit-subset/submissions", schema=submissions_schema)
+    reddit_comments = spark.read.json("reddit-subset/comments", schema=comments_schema)
+    
+    # Get anime releases from MyAnimeList #
+    
+    # Need to get anime releases manually as there is no formula to it
+    jujutsu_kaisen = insert_jikan_anime(40748)
+    my_hero_academia_fifth = insert_jikan_anime(41587)
+    spy_x_family = insert_jikan_anime(50265)
+    # JikanPy has a rate limit of 3 requests per second and won't work with more than 3 requests unless we wait/sleep for a second
+    rezero_season_two = insert_jikan_anime(39587,1)
+    kaguya_sama_ultra_romantic = insert_jikan_anime(43608,1)
+    demon_slayer_entertainment_district = insert_jikan_anime(47778,1)
+    attack_on_titan_final_season_two = insert_jikan_anime(48583,1)
+    komi_san = insert_jikan_anime(48926,1)
+    
+    # Format: Subreddit_name: MyAnimeList data
+    anime_list = [
+        ("JuJutsuKaisen", jujutsu_kaisen),
+        ("BokuNoHeroAcademia", my_hero_academia_fifth),
+        ("SpyxFamily", spy_x_family),
+        ("Re_Zero", rezero_season_two),
+        ("Kaguya_sama", kaguya_sama_ultra_romantic),
+        ("KimetsuNoYaiba", demon_slayer_entertainment_district),
+        ("ShingekiNoKyojin", attack_on_titan_final_season_two),
+        ("Komi_san", komi_san)
+    ]
+    
+    # 2 loops are needed to filter the data from JikanPy to get release dates as the library does not have any built in functions to handle this type of task
+    anime_list = [(subreddit, filter_date(data["data"]["aired"]["from"])) 
+                  for subreddit, data in anime_list]
+    
+    anime_list = [(subreddit, year, month) for subreddit, (year, month) in anime_list]
+    
+    anime_df = spark.createDataFrame(anime_list, schema=anime_schema).cache()
+    # anime_df.show()
+    
+    # print(anime_list)
     reddit_submissions = spark.read.json(reddit_submissions_path, schema=submissions_schema)
     reddit_comments = spark.read.json(reddit_comments_path, schema=comments_schema)
     
-    subs = []
-    # subs = ['Genealogy', 'xkcd', 'optometry', 'Cameras', 'scala']
-    subs = list(map(functions.lit, subs))
-    
+    subs = anime_df.select(functions.collect_list(anime_df.subreddit)).first()[0] # get the subreddits from my dataframe
+    # print(subs)
     reddit_submissions.where(reddit_submissions['subreddit'].isin(subs)) \
-        .where(reddit_submissions['year'] == 2022) \
+        .where(reddit_submissions['year'] >= 2020 & reddit_submissions['month'] >= 6) \
         .write.json(output + '/submissions', mode='overwrite', compression='gzip')
+    
     reddit_comments.where(reddit_comments['subreddit'].isin(subs)) \
-        .where(reddit_comments['year'] == 2022) \
+        .where(reddit_comments['year'] >= 2020 & reddit_comments['month'] >= 6) \
         .write.json(output + '/comments', mode='overwrite', compression='gzip')
     
 main()
