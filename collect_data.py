879b35abef0084086b078b0dd54b66e8e08096f8
Lucas Lee <ldl5@sfu.ca>
2023-07-31 20:49:01 -0700

    Filtered for previous and next months, including previous and next years if needed

--- collect_data.py
+++ collect_data.py
@@ -2,85 +2,12 @@ import sys
 import time
 from jikanpy import Jikan
 assert sys.version_info >= (3, 8)
-from pyspark.sql import SparkSession, functions, types, Row
+from pyspark.sql import SparkSession, functions, Row
+import schema
 
 reddit_submissions_path = '/courses/datasets/reddit_submissions_repartitioned/'
 reddit_comments_path = '/courses/datasets/reddit_comments_repartitioned/'
 output = 'reddit-subset'
-
-comments_schema = types.StructType([
-    types.StructField('archived', types.BooleanType()),
-    types.StructField('author', types.StringType()),
-    types.StructField('author_flair_css_class', types.StringType()),
-    types.StructField('author_flair_text', types.StringType()),
-    types.StructField('body', types.StringType()),
-    types.StructField('controversiality', types.LongType()),
-    types.StructField('created_utc', types.StringType()),
-    types.StructField('distinguished', types.StringType()),
-    types.StructField('downs', types.LongType()),
-    types.StructField('edited', types.StringType()),
-    types.StructField('gilded', types.LongType()),
-    types.StructField('id', types.StringType()),
-    types.StructField('link_id', types.StringType()),
-    types.StructField('name', types.StringType()),
-    types.StructField('parent_id', types.StringType()),
-    types.StructField('retrieved_on', types.LongType()),
-    types.StructField('score', types.LongType()),
-    types.StructField('score_hidden', types.BooleanType()),
-    types.StructField('subreddit', types.StringType()),
-    types.StructField('subreddit_id', types.StringType()),
-    types.StructField('ups', types.LongType()),
-    types.StructField('year', types.IntegerType()),
-    types.StructField('month', types.IntegerType()),
-])
-
-submissions_schema = types.StructType([
-    types.StructField('archived', types.BooleanType()),
-    types.StructField('author', types.StringType()),
-    types.StructField('author_flair_css_class', types.StringType()),
-    types.StructField('author_flair_text', types.StringType()),
-    types.StructField('created', types.LongType()),
-    types.StructField('created_utc', types.StringType()),
-    types.StructField('distinguished', types.StringType()),
-    types.StructField('domain', types.StringType()),
-    types.StructField('downs', types.LongType()),
-    types.StructField('edited', types.BooleanType()),
-    types.StructField('from', types.StringType()),
-    types.StructField('from_id', types.StringType()),
-    types.StructField('from_kind', types.StringType()),
-    types.StructField('gilded', types.LongType()),
-    types.StructField('hide_score', types.BooleanType()),
-    types.StructField('id', types.StringType()),
-    types.StructField('is_self', types.BooleanType()),
-    types.StructField('link_flair_css_class', types.StringType()),
-    types.StructField('link_flair_text', types.StringType()),
-    types.StructField('media', types.StringType()),
-    types.StructField('name', types.StringType()),
-    types.StructField('num_comments', types.LongType()),
-    types.StructField('over_18', types.BooleanType()),
-    types.StructField('permalink', types.StringType()),
-    types.StructField('quarantine', types.BooleanType()),
-    types.StructField('retrieved_on', types.LongType()),
-    types.StructField('saved', types.BooleanType()),
-    types.StructField('score', types.LongType()),
-    types.StructField('secure_media', types.StringType()),
-    types.StructField('selftext', types.StringType()),
-    types.StructField('stickied', types.BooleanType()),
-    types.StructField('subreddit', types.StringType()),
-    types.StructField('subreddit_id', types.StringType()),
-    types.StructField('thumbnail', types.StringType()),
-    types.StructField('title', types.StringType()),
-    types.StructField('ups', types.LongType()),
-    types.StructField('url', types.StringType()),
-    types.StructField('year', types.IntegerType()),
-    types.StructField('month', types.IntegerType()),
-])
-
-anime_schema = types.StructType([
-    types.StructField('subreddit', types.StringType()),
-    types.StructField('year', types.IntegerType()),
-    types.StructField('month', types.IntegerType()),
-])
 
 def insert_jikan_anime(code, wait_time=0):
     jikan = Jikan()
@@ -92,16 +19,13 @@ def filter_date(date):
     date = date.split("-")
     year = int(date[0])
     month = int(date[1])
-    return year, month
+    day = int((date[2].split("T"))[0])
+    return year, month, day
 
 def main():
     spark = SparkSession.builder.appName("reddit data").getOrCreate()
     assert spark.version >= '3.2'
     spark.sparkContext.setLogLevel('WARN')
-    
-    # Testing for reddit-subset in local data
-    reddit_submissions = spark.read.json("reddit-subset/submissions", schema=submissions_schema)
-    reddit_comments = spark.read.json("reddit-subset/comments", schema=comments_schema)
     
     # Get anime releases from MyAnimeList #
     
@@ -132,17 +56,29 @@ def main():
     anime_list = [(subreddit, filter_date(data["data"]["aired"]["from"])) 
                   for subreddit, data in anime_list]
     
-    anime_list = [(subreddit, year, month) for subreddit, (year, month) in anime_list]
+    anime_list = [(subreddit, year, month, day) for subreddit, (year, month, day) in anime_list]
     
-    anime_df = spark.createDataFrame(anime_list, schema=anime_schema).cache()
-    # anime_df.show()
+    anime_df = spark.createDataFrame(anime_list, schema=schema.anime_schema).cache()
     
-    # print(anime_list)
-    reddit_submissions = spark.read.json(reddit_submissions_path, schema=submissions_schema)
-    reddit_comments = spark.read.json(reddit_comments_path, schema=comments_schema)
+    # Get the previous month and year, if possible, if an anime was released within 10 days of the beginning of the month
+    anime_df = anime_df.withColumn("previous_month", (functions.when(functions.col("day") <= 10, functions.col("month") - 1).otherwise(None))) # get the previous month
+    anime_df = anime_df.withColumn("previous_year", functions.when(functions.col("previous_month") == 0, functions.col("year") - 1).otherwise(None))
+    anime_df = anime_df.withColumn("previous_month", (functions.when(functions.col("previous_month") == 0, 12)))
+    
+    # Get the next month and year, if possible, if an anime was released within 21 days of the end of the month
+    anime_df = anime_df.withColumn("next_month", (functions.when(functions.col("day") >= 21, functions.col("month") + 1).otherwise(None))) # get the next month
+    anime_df = anime_df.withColumn("next_year", functions.when(functions.col("next_month") == 13, functions.col("year") + 1).otherwise(None))
+    anime_df = anime_df.withColumn("next_month", (functions.when(functions.col("next_month") == 13, 1)))
+    
+    
+    # Testing for reddit-subset in local data
+    reddit_submissions = spark.read.json("reddit-subset/submissions", schema=schema.submissions_schema)
+    reddit_comments = spark.read.json("reddit-subset/comments", schema=schema.comments_schema)
+    # reddit_submissions = spark.read.json(reddit_submissions_path, schema=submissions_schema)
+    # reddit_comments = spark.read.json(reddit_comments_path, schema=comments_schema)
     
     subs = anime_df.select(functions.collect_list(anime_df.subreddit)).first()[0] # get the subreddits from my dataframe
-    # print(subs)
+    
     reddit_submissions.where(reddit_submissions['subreddit'].isin(subs)) \
         .where(reddit_submissions['year'] >= 2020 & reddit_submissions['month'] >= 6) \
         .write.json(output + '/submissions', mode='overwrite', compression='gzip')
