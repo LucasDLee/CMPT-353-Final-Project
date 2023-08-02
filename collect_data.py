3080b7569ce7a9ad3d666945f7f835d576325f97
ikec <tszmingjack@gmail.com>
2023-08-02 10:13:37 -0700

    cleaning up code in collect_data.py

--- collect_data.py
+++ collect_data.py
@@ -82,20 +82,24 @@ anime_schema = types.StructType([
     types.StructField('month', types.IntegerType()),
 ])
 
-def insert_jikan_anime(code, wait_time=0):
+# JikanPy has a rate limit of 3 requests per second and won't work with more than 3 requests unless we wait/sleep for a second
+def insert_jikan_anime(code, wait_time=1.05/3):
     jikan = Jikan()
     anime = jikan.anime(code)
     time.sleep(wait_time)
     return anime
 
-def filter_date(date):
-    date = date.split("-")
+def f(anime):
+    name = anime[0]
+    jj = anime[1]
+    aired = jj["data"]["aired"]["from"]
+    date = aired.split("-")
     year = int(date[0])
     month = int(date[1])
-    return year, month
+    return name, year, month
 
 def main():
-    spark = SparkSession.builder.appName("reddit data").getOrCreate()
+    spark = SparkSession.Builder.appName(SparkSession.builder, "reddit data").getOrCreate()
     assert spark.version >= '3.2'
     spark.sparkContext.setLogLevel('WARN')
     
@@ -103,21 +107,22 @@ def main():
     reddit_submissions = spark.read.json("reddit-subset/submissions", schema=submissions_schema)
     reddit_comments = spark.read.json("reddit-subset/comments", schema=comments_schema)
     
-    # Get anime releases from MyAnimeList #
-    
+    # Get anime releases from MyAnimeList
     # Need to get anime releases manually as there is no formula to it
     jujutsu_kaisen = insert_jikan_anime(40748)
     my_hero_academia_fifth = insert_jikan_anime(41587)
     spy_x_family = insert_jikan_anime(50265)
-    # JikanPy has a rate limit of 3 requests per second and won't work with more than 3 requests unless we wait/sleep for a second
-    rezero_season_two = insert_jikan_anime(39587,1)
-    kaguya_sama_ultra_romantic = insert_jikan_anime(43608,1)
-    demon_slayer_entertainment_district = insert_jikan_anime(47778,1)
-    attack_on_titan_final_season_two = insert_jikan_anime(48583,1)
+    rezero_season_two = insert_jikan_anime(39587)
+    kaguya_sama_ultra_romantic = insert_jikan_anime(43608)
+    demon_slayer_entertainment_district = insert_jikan_anime(47778)
+    attack_on_titan_final_season_two = insert_jikan_anime(48583)
     komi_san = insert_jikan_anime(48926,1)
     
     # Format: Subreddit_name: MyAnimeList data
-    anime_list = [
+    # 2 loops are needed to filter the data from JikanPy to get release dates as the library does not have any built in functions to handle this type of task
+
+    # ike: can do this in one like this
+    anime_list = list(map(f, [
         ("JuJutsuKaisen", jujutsu_kaisen),
         ("BokuNoHeroAcademia", my_hero_academia_fifth),
         ("SpyxFamily", spy_x_family),
@@ -126,23 +131,16 @@ def main():
         ("KimetsuNoYaiba", demon_slayer_entertainment_district),
         ("ShingekiNoKyojin", attack_on_titan_final_season_two),
         ("Komi_san", komi_san)
-    ]
-    
-    # 2 loops are needed to filter the data from JikanPy to get release dates as the library does not have any built in functions to handle this type of task
-    anime_list = [(subreddit, filter_date(data["data"]["aired"]["from"])) 
-                  for subreddit, data in anime_list]
-    
-    anime_list = [(subreddit, year, month) for subreddit, (year, month) in anime_list]
+    ]))
     
     anime_df = spark.createDataFrame(anime_list, schema=anime_schema).cache()
-    # anime_df.show()
+    anime_df.show()
     
     # print(anime_list)
     reddit_submissions = spark.read.json(reddit_submissions_path, schema=submissions_schema)
     reddit_comments = spark.read.json(reddit_comments_path, schema=comments_schema)
     
     subs = anime_df.select(functions.collect_list(anime_df.subreddit)).first()[0] # get the subreddits from my dataframe
-    # print(subs)
     reddit_submissions.where(reddit_submissions['subreddit'].isin(subs)) \
         .where(reddit_submissions['year'] >= 2020 & reddit_submissions['month'] >= 6) \
         .write.json(output + '/submissions', mode='overwrite', compression='gzip')
